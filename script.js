9b8ffc2ab5c162d2159ba5446db4b7af65ae52c6
blackanvilsoftworks <blackanvilsoftworks@gmail.com>
2025-08-23 13:04:33 -0300

    Se corrige disposición de campos input

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ contacForm.addEventListener('submit', function(e) {
     }
 
     whatsappLinkPhoneNumber = ` Link al WhatsApp: https://wa.me/549${cleanedPhoneNumber}`;
+    //whatsappLinkPhoneNumber = `https://wa.me/549${cleanedPhoneNumber}`;
 
     /*
     const message = `Hola,%20soy%20${encodeURIComponent(name)}.%20Te%20contacto%20desde%20la%20web%20de%20Calwill%20%26%20Fiorella`;
@@ -28,8 +29,12 @@ contacForm.addEventListener('submit', function(e) {
     const whatsappLink = `https://wa.me/54${phone}?text=${message}`;
     */
     
-    inputMessage.value += whatsappLinkPhoneNumber;
+    //inputMessage.value += whatsappLinkPhoneNumber;
     
+    inputName.value = cleanedName;
+    inputPhoneNumber.value = cleanedPhoneNumber;
+    inputMessage.value = cleanedMessage + whatsappLinkPhoneNumber;
+
     this.submit(); // Esto activa FormSubmit
     contacForm.reset();
     alert('Formulario enviado con éxito.');
