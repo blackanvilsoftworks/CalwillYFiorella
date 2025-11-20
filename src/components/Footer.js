dbc0772a4dea0705c469440ac3adce98e1f51287
blackanvilsoftworks <blackanvilsoftworks@gmail.com>
2025-11-19 23:34:43 -0300

    Se realiza un cambio menor

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -26,8 +26,7 @@ export default class Footer {
                         </div>                    
                     </div>
                 </div>
-            </div>
-        `;
+            </div>`;
     }
 
     createSocialMediaButton () {
